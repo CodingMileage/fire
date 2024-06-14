9c438f7b2bc38e68984b1a45ac6751a613fe3be3
CodingMileage <72987158+CodingMileage@users.noreply.github.com>
2024-06-14 05:17:29 -0500

    Music player with random background

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,33 +1,47 @@
-import React, { useRef, useState } from 'react';
-import { Howl } from 'howler';
-import music from './Music.mp3';
+import React, { useRef, useState, useEffect } from "react";
+import { Howl } from "howler";
+import MusicPlayer from "./MusicPlayer";
+import music from "./Music.mp3";
 
 const audioClip = [
-  { sound: "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3", label: 'Yea' },
+  {
+    sound:
+      "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3",
+    label: "Yea",
+  },
 ];
 
 const Music = () => {
   const [fileUpload, setFileUpload] = useState(null);
-  const [fileFormat, setFileFormat] = useState('');
+  const [fileFormat, setFileFormat] = useState("");
   const [rate, setRate] = useState(1.0);
   const [vol, setVol] = useState(0.5);
-
+  const [songLength, setSongLength] = useState(0);
+  const [title, setTitle] = useState("");
 
   const soundRef = useRef(null);
+
+  useEffect(() => {
+    if (soundRef && soundRef.current) {
+      setSongLength(soundRef.current.duration());
+    }
+  }, [soundRef]);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
       const fileUrl = URL.createObjectURL(file);
-      const fileExtension = file.name.split('.').pop();
+      const fileName = file.name;
+      const fileExtension = file.name.split(".").pop();
       setFileUpload(fileUrl);
       setFileFormat(fileExtension);
+      setTitle(fileName);
     }
   };
 
   const localPlay = (src, format) => {
     if (soundRef.current) {
-      soundRef.current.unload(); // Unload the previous sound if it exists
+      soundRef.current.unload();
     }
 
     soundRef.current = new Howl({
@@ -35,10 +49,22 @@ const Music = () => {
       format: [format],
       rate: 1.0,
       volume: vol,
+      onload: () => {
+        setSongLength(soundRef.current._duration);
+        // console.log(soundRef.current._duration % 60);
+      },
     });
 
     soundRef.current.play();
   };
+
+  // const sound = new Howl({
+  //   src,
+  //   format: [format],
+  //   onload: () => {
+  //     console.log("Great");
+  //   },
+  // });
 
   const pausePlay = () => {
     if (soundRef.current && soundRef.current.playing()) {
@@ -51,9 +77,9 @@ const Music = () => {
   const changeRate = (delta) => {
     if (soundRef.current) {
       const currentRate = soundRef.current.rate();
-      const newRate = Math.max(0, Math.min(1, currentRate + delta));
+      const newRate = Math.max(0, Math.min(2, currentRate + delta));
       soundRef.current.rate(newRate);
-      setRate(newRate)
+      setRate(newRate);
     }
   };
 
@@ -62,98 +88,75 @@ const Music = () => {
       const currentVolume = soundRef.current.volume();
       const newVolume = Math.max(0, Math.min(1, currentVolume + delta));
       soundRef.current.volume(newVolume);
-      setVol(newVolume)
+      setVol(newVolume);
     }
   };
 
-  const RenderButtonSound = () => {
-    return audioClip.map((soundObj, index) => (
-      <div className='flex flex-row justify-center p-3' key={index}>
-        <p>{soundObj.label}</p>
-        <button
-          onClick={() => { localPlay(soundObj.sound, 'mp3') }}
-          className='p-2 m-2 bg-purple-200 rounded-lg'
-        >
-          {soundObj.label}
-        </button>
-        {/* <p className='p-2 m-2 bg-blue-200 rounded-lg'>
-          {soundObj.sound.split('/').pop().split('.')[0]}
-        </p> */}
+  const durationn = () => {};
 
-        <div className='flex-center items-center'>
-          <div>
-            <h1 className='font-bold text-center'>Volume: {Math.round(vol * 100)}%</h1>
-            <button 
-              className='btnn px-4 py-1'
-              onClick={() => changeVolume(-0.1)}
-            >
-              -
-            </button>
+  // const RenderButtonSound = () => {
+  //   return audioClip.map((soundObj, index) => (
+  //     <div className="justify-center p-3 " key={index}>
+  //       <p>{soundObj.label}</p>
+  //       <button
+  //         onClick={() => { localPlay(soundObj.sound, 'mp3') }}
+  //         className='p-2 m-2 bg-purple-200 rounded-lg'
+  //       >
+  //         {soundObj.label}
+  //       </button>
 
-            <button 
-              className='btnn px-4 py-1'
-              onClick={() => changeVolume(0.05)}
-            >
-              +
-            </button>
-          </div>
-          <div>
-          <h1 className='font-bold text-center'>Rate: {Math.round(rate * 100)}%</h1>
-          <button 
-            className='btnn px-4 py-1'
-            onClick={() => changeRate(-0.05)}
-          >
-            -
-        </button>
+  //       <p className='p-2 m-2 bg-blue-200 rounded-lg'>
+  //         {soundObj.sound.split('/').pop().split('.')[0]}
+  //       </p>
 
-        <button 
-          className='btnn px-4 py-1'
-          onClick={() => changeRate(0.05)}
-          >
-            +
-        </button>
-          </div>
-        </div>
+  //       {console.log(soundRef)}
 
-        <button
-          onClick={pausePlay}
-          className='btnn'
-        >
-          Pause
-        </button>
-{/* 
-        <button
-          onClick={() => changeRate((prompt("Enter value")))}
-          className=' btnn bg-green-200 hover:bg-green-600'
-        >
-          Change Rate
-        </button> */}
-        
-        <div className='flex flex-col items-center justify-center p-3'>
-        <input 
-          type="file" 
-          accept="audio/*" 
-          onChange={handleFileChange} 
-          className='p-2 m-2 bg-gray-200 rounded-lg'
+  //       <button onClick={pausePlay} className="btnn">
+  //         Pause
+  //       </button>
+
+  //       <button
+  //         onClick={() => changeRate((prompt("Enter value")))}
+  //         className='bg-green-200 btnn hover:bg-green-600'
+  //       >
+  //         Change Rate
+  //       </button>
+  //     </div>
+  //   ));
+  // };
+
+  return (
+    <div className="card">
+      <MusicPlayer
+        pausePlay={pausePlay}
+        vol={vol}
+        rate={rate}
+        changeRate={changeRate}
+        changeVolume={changeVolume}
+        soundRef={soundRef}
+        songLength={songLength}
+        title={title}
+      />
+
+      <div className="flex flex-col items-center justify-center p-3">
+        <input
+          type="file"
+          accept="audio/*"
+          onChange={handleFileChange}
+          className="p-2 m-2 bg-gray-200 rounded-lg"
         />
         {fileUpload && (
           <button
-            onClick={() => { localPlay(fileUpload, fileFormat) }}
-            className='p-2 m-2 bg-blue-200 rounded-lg'
+            onClick={() => {
+              localPlay(fileUpload, fileFormat);
+            }}
+            className="p-2 m-2 bg-blue-200 rounded-lg"
           >
             Play Uploaded File
           </button>
         )}
       </div>
-      </div>
-    ));
-  };
-
-  return (
-    <div className='card'>
-      {/* <h1 className='bg-red-200 p-2 m-2 rounded-lg'>Music</h1> */}
-      {RenderButtonSound()}
-      
+      {/* {RenderButtonSound()} */}
     </div>
   );
 };
